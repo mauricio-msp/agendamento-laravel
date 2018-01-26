16dcdf4c851452fc0c4cddce455ca5788c28751e
José Carlos Carneiro <jccarneiros@gmail.com>
2018-01-26 16:38:03 -0200

    - created file phpcs.xml

--- Modules/Backend/Providers/BackendServiceProvider.php
+++ Modules/Backend/Providers/BackendServiceProvider.php
@@ -49,7 +49,8 @@ class BackendServiceProvider extends ServiceProvider
             __DIR__.'/../Config/config.php' => config_path('backend.php'),
         ], 'config');
         $this->mergeConfigFrom(
-            __DIR__.'/../Config/config.php', 'backend'
+            __DIR__.'/../Config/config.php',
+            'backend'
         );
     }
 
@@ -66,7 +67,7 @@ class BackendServiceProvider extends ServiceProvider
 
         $this->publishes([
             $sourcePath => $viewPath
-        ],'views');
+        ], 'views');
 
         $this->loadViewsFrom(array_merge(array_map(function ($path) {
             return $path . '/modules/backend';
