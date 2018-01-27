f8b88b404f8b268fae6d2e4b3ac03057ab232560
José Carlos Carneiro <jccarneiros@gmail.com>
2018-01-27 03:05:14 -0200

    - update views

--- Modules/Backend/Resources/views/layouts/master.blade.php
+++ Modules/Backend/Resources/views/layouts/master.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="{{url('assets/images/favicon.ico')}}">
     <!-- Bootstrap core CSS -->
     <link href="{{url('assets/css/app.css')}}" rel="stylesheet">
+
 </head>
 <body>
 @yield('content')
