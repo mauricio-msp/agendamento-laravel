0d0c1443b12e3a81946b7ca851f7bd0dfedca531
vagrant <vagrant@homestead>
2018-01-27 20:38:22 +0000

    Alteração na tela de login backend

--- Modules/Backend/Resources/views/layouts/master.blade.php
+++ Modules/Backend/Resources/views/layouts/master.blade.php
@@ -1,20 +1,21 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="{{url('assets/images/favicon.ico')}}">
-    <!-- Bootstrap core CSS -->
-    <link href="{{url('assets/css/app.css')}}" rel="stylesheet">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="{{url('assets/images/favicon.ico')}}">
+        <!-- Bootstrap core CSS -->
+        <link href="{{url('assets/css/app.css')}}" rel="stylesheet">
 
-</head>
-<body>
-@yield('content')
-<script type="text/javascript" src="{{url('assets/js/jquery-3.2.1.slim.min.js')}}"></script>
-<script type="text/javascript" src="{{url('assets/js/app.js')}}"></script>
-</body>
+    </head>
+    <body>
+        @yield('content')
+        
+        <script type="text/javascript" src="{{url('assets/js/jquery-3.2.1.slim.min.js')}}"></script>
+        <script type="text/javascript" src="{{url('assets/js/app.js')}}"></script>
+    </body>
 </html>
